f42f02034678ff1d175effc3adce2256c5c1f45c
saidi <saidi.morthala@volantetech.com>
2025-05-19 19:59:32 +0530

    sss

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "lab-vpc" {
   cidr_block = var.dev_cidr_block
-  tags = var.dev_tags
+  
   instance_tenancy = "default"
   
   tags = merge(
     {
       "Name" = var.dev_vpc_name
     },
-  var.dr-tags)
+  var.dev_tags)
 }
