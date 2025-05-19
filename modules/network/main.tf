4d915dc63c972aa54ebb50ec313f9ba33ca46720
saidi <saidi.morthala@volantetech.com>
2025-05-19 22:19:46 +0530

    sss

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -91,7 +91,7 @@ resource "aws_internet_gateway" "dev-igw" {
 ###################EC2 - RT (Public & Private) #############
 resource "aws_route_table" "dev-ec2-public-rt-02" {
   vpc_id = aws_vpc.lab-vpc.id
-  count = length(var.public_subnet_cidrs)
+  /*count = length(var.public_subnet_cidrs)*/
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -100,24 +100,25 @@ resource "aws_route_table" "dev-ec2-public-rt-02" {
 
  tags = merge(
     {
-      "Name" = "${var.dev_vpc_name}-${var.env}-public-ec2-rt${count.index + 1}"
+      "Name" = "${var.dev_vpc_name}-${var.env}-public-ec2-rt-02"
+      /*"Name" = "${var.dev_vpc_name}-${var.env}-public-ec2-rt${count.index + 1}"*/
     },
   var.dev_tags)
 }
 #####################################################################################
 resource "aws_route_table" "dev-ec2-private-rt-01" {
-  count = length(var.private_subnet_cidrs )
-  vpc_id = aws_vpc.lab-vpc.id
- 
 
-  /*route {
+  vpc_id = aws_vpc.lab-vpc.id
+  /*count = length(var.private_subnet_cidrs )
+  route {
       cidr_block = element(var.private_subnet_cidrs, count.index)
     gateway_id = aws_internet_gateway.dev-igw.id
   }*/
 
  tags = merge(
     {
-      "Name" = "${var.dev_vpc_name}-${var.env}-private-ec2-rt${count.index + 1}"
+      "Name" = "${var.dev_vpc_name}-${var.env}-private-ec2-rt-01"
+      /*"Name" = "${var.dev_vpc_name}-${var.env}-private-ec2-rt${count.index + 1}"*/
     },
   var.dev_tags)
 }
@@ -127,33 +128,33 @@ resource "aws_route_table" "dev-ec2-private-rt-01" {
 
 resource "aws_route_table" "dev-private-rt-rmq-04" {
   vpc_id = aws_vpc.lab-vpc.id
-  count = length(var.private_subnet_rmq_cidrs)
+  /*count = length(var.private_subnet_rmq_cidrs)
 
-  /*route {
+  route {
     cidr_block = element(var.private_subnet_rmq_cidrs, count.index)
     gateway_id = aws_internet_gateway.dev-igw.id
   }*/
 
  tags = merge(
     {
-      "Name" = "${var.dev_vpc_name}-${var.env}-private-rmq-rt${count.index + 1}"
+      "Name" = "${var.dev_vpc_name}-${var.env}-private-rmq-rt-03"
+      /*"Name" = "${var.dev_vpc_name}-${var.env}-private-rmq-rt${count.index + 1}"*/
     },
   var.dev_tags)
 }
 
 resource "aws_route_table" "dev-private-rt-amq-03" {
-  count = length(var.private_subnet_amq_cidrs )
   vpc_id = aws_vpc.lab-vpc.id
- 
-
-  /*route {
-      cidr_block = element(var.private_subnet_amq_cidrs, count.index)
+  /*count = length(var.private_subnet_amq_cidrs )
+  route {
+    cidr_block = element(var.private_subnet_amq_cidrs, count.index)
     gateway_id = aws_internet_gateway.dev-igw.id
   }*/
 
  tags = merge(
     {
-      "Name" = "${var.dev_vpc_name}-${var.env}-private-amq-rt${count.index + 1}"
+      "Name" = "${var.dev_vpc_name}-${var.env}-private-amq-rt-04"
+      /*"Name" = "${var.dev_vpc_name}-${var.env}-private-amq-rt${count.index + 1}"*/
     },
   var.dev_tags)
 }
@@ -161,27 +162,31 @@ resource "aws_route_table" "dev-private-rt-amq-03" {
 ##########################################################################################
 ###################EC2 RT Association #############
 resource "aws_route_table_association" "dev-ec2-sn-rt-pub-association" {
-  count = length(var.public_subnet_cidrs)
+  /*count = length(var.public_subnet_cidrs)*/
   subnet_id      = aws_subnet.dev_public_ec2_subnet-02[count.index].id
-  route_table_id = aws_route_table.dev-ec2-public-rt-02[count.index].id
+  route_table_id = aws_route_table.dev-ec2-public-rt-02.id
+  /*route_table_id = aws_route_table.dev-ec2-public-rt-02[count.index].id*/
 }
 
 resource "aws_route_table_association" "dev-ec2-sn-rt-pvt-association" {
-  count = length(var.public_subnet_cidrs)
+  /*count = length(var.public_subnet_cidrs)*/
   subnet_id      = aws_subnet.dev_private_ec2_subnet-01[count.index].id
-  route_table_id = aws_route_table.dev-ec2-private-rt-01[count.index].id
+  route_table_id = aws_route_table.dev-ec2-private-rt-01.id
+  /*route_table_id = aws_route_table.dev-ec2-private-rt-01[count.index].id*/
 }
 
 ##########################################################################################
 ###################RMQ & AMQ RT Association #############
 resource "aws_route_table_association" "dev-ec2-sn-rmq-rt-pub-association" {
-  count = length(var.public_subnet_cidrs)
+  /*count = length(var.public_subnet_cidrs)*/
   subnet_id      = aws_subnet.dev_private_rmq_subnet-04[count.index].id
-  route_table_id = aws_route_table.dev-private-rt-rmq-04[count.index].id
+  route_table_id = aws_route_table.dev-private-rt-rmq-04.id
+  /*route_table_id = aws_route_table.dev-private-rt-rmq-04[count.index].id*/
 }
 
 resource "aws_route_table_association" "dev-ec2-sn-amq-rt-pvt-association" {
-  count = length(var.public_subnet_cidrs)
+  /*count = length(var.public_subnet_cidrs)*/
   subnet_id      = aws_subnet.dev_private_amq_subnet-03[count.index].id
-  route_table_id = aws_route_table.dev-private-rt-amq-03[count.index].id
+  /*route_table_id = aws_route_table.dev-private-rt-amq-03[count.index].id*/
+  route_table_id = aws_route_table.dev-private-rt-amq-03.id
 }
