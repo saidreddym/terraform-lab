e17c906255ebcdafc0f419d2aac59a6f1d04034b
saidi <saidi.morthala@volantetech.com>
2025-05-19 15:45:02 +0530

    test

--- modules/modules/sg/sg.tf
+++ modules/modules/sg/sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_all" {
-  name        = "${var.vpc_name}"-allow-all
+  name        = "${var.vpc_name}"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
 
