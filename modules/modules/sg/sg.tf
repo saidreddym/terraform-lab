567958a85c6ba392c9e0a2a3cf949da8736cbae3
saidi <saidi.morthala@volantetech.com>
2025-05-19 15:42:35 +0530

    test

--- modules/modules/sg/sg.tf
+++ modules/modules/sg/sg.tf
@@ -13,13 +13,7 @@ resource "aws_security_group" "allow_all" {
     
     }
 }
-  egress = {
-     from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]   # Use with caution!
-    }
-
+ 
   
 
     tags = {
