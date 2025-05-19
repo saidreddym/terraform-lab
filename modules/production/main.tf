066991f70042923adb0a39ecb28b4bb4b7f13e26
saidi <saidi.morthala@volantetech.com>
2025-05-19 13:57:51 +0530

    xx

--- modules/production/main.tf
+++ modules/production/main.tf
@@ -1,6 +1,6 @@
 module "prod_vpc_1" {
     source = "../modules/vpc"
-    vpc_name = "dev-vpc"
+    vpc_name = "prod-vpc"
     vpc_cidr = "192.168.0.0/16"
     environment = "dev"
     public_cidr_block = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
