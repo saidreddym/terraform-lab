08321304ecd454e665a0573a6e18c7d78a8cca41
saidi <saidi.morthala@volantetech.com>
2025-05-19 21:33:47 +0530

    sss

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -148,7 +148,7 @@ resource "aws_route_table" "dev-private-rt-amq-04" {
 
 ##########################################################################################
 ###################EC2 RT Association #############
-resource "aws_route_table_association" "dev-ec2-sn-rt-pub-ass" {
+resource "aws_route_table_association" "dev-ec2-sn-rt-pub-association" {
   count = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.dev_public_ec2_subnet-02[count.index].id
   route_table_id = aws_route_table.dev-ec2-public-rt-02[count.index].id
