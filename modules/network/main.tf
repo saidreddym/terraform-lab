cb7173511ee9943ba8244900a4f0bc77302b9072
saidi <saidi.morthala@volantetech.com>
2025-05-19 21:48:30 +0530

    sss

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -113,7 +113,7 @@ resource "aws_route_table" "dev-ec2-private-rt-01" {
 ###################MQ & AMQ - RT (Public & Private) #############
 
 
-resource "aws_route_table" "dev-private-rt-rmq-03" {
+resource "aws_route_table" "dev-private-rt-rmq-04" {
   vpc_id = aws_vpc.lab-vpc.id
   count = length(var.private_subnet_rmq_cidrs)
 
@@ -129,7 +129,7 @@ resource "aws_route_table" "dev-private-rt-rmq-03" {
   var.dev_tags)
 }
 
-resource "aws_route_table" "dev-private-rt-amq-04" {
+resource "aws_route_table" "dev-private-rt-amq-03" {
   count = length(var.private_subnet_amq_cidrs )
   vpc_id = aws_vpc.lab-vpc.id
  
@@ -157,19 +157,19 @@ resource "aws_route_table_association" "dev-ec2-sn-rt-pub-association" {
 resource "aws_route_table_association" "dev-ec2-sn-rt-pvt-association" {
   count = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.dev_private_ec2_subnet-01[count.index].id
-  route_table_id = aws_route_table.dev_private_ec2_subnet[count.index].id
+  route_table_id = aws_route_table.dev-ec2-private-rt-01[count.index].id
 }
 
 ##########################################################################################
 ###################RMQ & AMQ RT Association #############
-resource "aws_route_table_association" "dev-ec2-sn-rt-pub-association" {
+resource "aws_route_table_association" "dev-ec2-sn-rmq-rt-pub-association" {
   count = length(var.public_subnet_cidrs)
-  subnet_id      = aws_subnet.dev_public_ec2_subnet-02[count.index].id
-  route_table_id = aws_route_table.dev-ec2-public-rt-02[count.index].id
+  subnet_id      = aws_subnet.dev_private_rmq_subnet-04[count.index].id
+  route_table_id = aws_route_table.dev-private-rt-rmq-04[count.index].id
 }
 
-resource "aws_route_table_association" "dev-ec2-sn-rt-pvt-association" {
+resource "aws_route_table_association" "dev-ec2-sn-amq-rt-pvt-association" {
   count = length(var.public_subnet_cidrs)
-  subnet_id      = aws_subnet.dev_private_ec2_subnet-01[count.index].id
-  route_table_id = aws_route_table.dev-ec2-private-rt-01[count.index].id
+  subnet_id      = aws_subnet.dev_private_amq_subnet-03[count.index].id
+  route_table_id = aws_route_table.dev-private-rt-amq-03[count.index].id
 }
