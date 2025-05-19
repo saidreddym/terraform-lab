9ef9f7beb20166d3d4d93184756fd3d499783cc8
saidi <saidi.morthala@volantetech.com>
2025-05-19 15:22:31 +0530

    xxxx

--- modules/development/main.tf
+++ modules/development/main.tf
@@ -10,9 +10,10 @@ module "dev_vpc_1" {
 }
 
    module "security_group" {
+    depends_on = [ module.dev_vpc_1 ]
   source         = "../modules/sg"
-  vpc_name       = module.dev_vpc_1.vpc_name.name
-  vpc_id         = module.dev_vpc_1.vpc_id.id
+  vpc_name       = module.dev_vpc_1.vpc_name
+  vpc_id         = module.dev_vpc_1.vpc_id
   ingress_value  = ["80", "8080", "443", "8443", "22", "3306", "1900"]
-  environment    = module.dev_vpc_1.environment.name
+  environment    = module.dev_vpc_1.environment
 }
