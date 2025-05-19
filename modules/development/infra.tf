cb85de3a524a8c9c1f00fb5fa86b607fca87222f
saidi <saidi.morthala@volantetech.com>
2025-05-19 13:43:22 +0530

    s

--- modules/development/infra.tf
+++ modules/development/infra.tf
@@ -1,6 +1,6 @@
 module "dev_vpc_1" {
     source = "../modules/vpc"
-    vpc_name = "dev_vpc_1"
+    vpc_name = "dev-vpc"
     vpc_cidr = "10.0.0.0/16"
     environment = "dev"
     public_cidr_block = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
