298c11f3ab8753537ff3b08878ffcf41f6724f75
saidi <saidi.morthala@volantetech.com>
2025-05-19 23:34:53 +0530

    sss

--- main.tf
+++ main.tf
@@ -13,7 +13,8 @@ module "network" {
 }
 module "s3" {
     source = "./modules/s3"
-    
-  
+    env                      = var.env
+    dev_tags                 = var.dev_tags
+    dev_vpc_name             = var.dev_vpc_name
 }
 
