896aaf6d9be95c2b92c240051f541392daf9afe4
saidi <saidi.morthala@volantetech.com>
2025-05-20 09:24:13 +0530

    sss

---
+++ modules/compute/main.tf
@@ -0,0 +1,16 @@
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "dev_ec2_key" {
+  key_name   = "dev-ec2-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+
+  tags = merge(
+    {
+      Name = "${var.dev_vpc_name}-${var.env}-key-pair-for-ec2-mq"
+    },
+    var.dev_tags
+  )
+}
---
+++ modules/compute/variables.tf
@@ -0,0 +1,3 @@
+variable "dev_tags" {}
+variable "dev_vpc_name" {}
+variable "env" {}
