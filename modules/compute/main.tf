87081034b4c58be05d585c72a5a99991cbbea15b
saidi <saidi.morthala@volantetech.com>
2025-05-20 15:57:02 +0530

    xx

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -21,7 +21,7 @@ resource "local_file" "key_pair_local" {
   file_permission = "0600"
 }
 resource "aws_instance" "ec2_public" {
-  depends_on = [ aws_key_pair.dev_ec2_key ]
+  
   count 							        = var.public_ec2_count
   ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
@@ -43,22 +43,6 @@ resource "aws_instance" "ec2_public" {
     volume_type = "gp2"
     delete_on_termination = true
     encrypted = true
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install -y httpd",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd",
-      "echo '<h1>Hello from Terraform</h1>' | sudo tee /var/www/html/index.html"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = "${path.module}/key_pair_local"  # Use your private key path
-      host        = self.public_ip
-    }
   }
   user_data = <<-EOF
               #!/bin/bash
