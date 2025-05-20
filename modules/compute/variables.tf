a3ce731abd0ef3bd09d2876f56c8bd8221c0c49d
saidi <saidi.morthala@volantetech.com>
2025-05-20 14:19:16 +0530

    xx

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -5,5 +5,7 @@ variable "ec2_ami" {}
 variable "ec2_instance_type" {}
 variable "ec2_subnet_id" {}
 variable "public_ec2_count" {}
-variable "ec2_public_security_group" {}
+variable "ec2_public_security_group" {
+     type = list(string)
+}
 
