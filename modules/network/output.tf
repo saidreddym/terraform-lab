ddf9e6c489fe18d75da62c508e0ab141a49e9271
saidi <saidi.morthala@volantetech.com>
2025-05-20 13:06:02 +0530

    xx

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -11,7 +11,7 @@ output "dev_public_ec2_subnet-02_output_02" {
 }
 output "dev_public_ec2_subnet-02_output_03" {
     description = "Outputs the ID of the selected public EC2 subnet"
-    value = "${aws_subnet.dev_public_ec2_subnet-02[1]}"
+    value = "${aws_subnet.dev_public_ec2_subnet-02[2]}"
 }
 output "dev_ec2_pub_sg_01" {
     value = aws_security_group.ec2_public_sg_01.id
