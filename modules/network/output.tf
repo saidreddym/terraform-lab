aff8c87e7412533c5f3cf98ee3080a22449be665
saidi <saidi.morthala@volantetech.com>
2025-05-20 10:44:31 +0530

    xx

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -2,7 +2,7 @@ output "aws_vpc" {
     value = aws_vpc.lab-vpc.id
 }
 output "dev_public_ec2_subnet-02_output" {
-
-    value = aws_subnet.dev_public_ec2_subnet-02[count.index].id
+    description = "Outputs the ID of the selected public EC2 subnet"
+    value = aws_subnet.dev_public_ec2_subnet-02[var.subnet_index].id
   
 }
