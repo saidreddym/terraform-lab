022a339ba1aa49c79b07f0320d6f9a11d992ba25
saidi <saidi.morthala@volantetech.com>
2025-05-21 22:14:02 +0530

    xx

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -18,9 +18,9 @@ resource "aws_subnet" "dev_private_ec2_subnet-01" {
   cidr_block = element(var.private_subnet_cidrs, count.index)
   availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = false
-  lifecycle {
+  /*lifecycle {
     create_before_destroy = true
-  }
+  }*/
 
 
   tags = merge(
