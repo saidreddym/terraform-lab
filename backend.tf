0363cec89361f03d75b777cc5b332502fa348485
saidi <saidi.morthala@volantetech.com>
2025-05-21 22:44:29 +0530

    xx

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "prodtfstbktterraform"
-    key    = "terraform.tfstate"
+    key    = "preprod/volpayapp.tfstate"
     region = "us-east-1"
     encrypt = true
   }
